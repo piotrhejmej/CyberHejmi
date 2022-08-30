30c87215c6a4721297ccfd4cb6e8574a8f3043e8
Piotrek Hejmej <piotrek10h@gmail.com>
2022-08-30 21:20:18 +0200

    Init

---
+++ CyberHejmiBot/Business/Events/EventListener.cs
@@ -0,0 +1,26 @@
+﻿using CyberHejmiBot.Business.Events.GuildEvents;
+using Discord.WebSocket;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberHejmiBot.Business.Events
+{
+    internal class EventListener: IEventListener
+    {
+        private readonly IGuildEventsListener GuildEventsListener;
+
+        public EventListener(IGuildEventsListener guildEventsListener)
+        {
+            GuildEventsListener = guildEventsListener;
+        }
+
+        public async Task StartAsync()
+        {
+            await GuildEventsListener.StartAsync();
+        }
+    }
+}
--- CyberHejmiBot/Program.cs
+++ CyberHejmiBot/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 public class Program
 {
-
     private readonly IServiceProvider _serviceProvider;
 
     public Program()
@@ -18,57 +17,12 @@ public class Program
 
     static void Main(string[] args)
         => new Program().RunAsync(args).GetAwaiter().GetResult();
-        
-    private DiscordSocketClient Client;
 
     public async Task RunAsync(string[] args)
     {
         var startup = _serviceProvider.GetRequiredService<IStartup>();
         await startup.Init();
 
-        // Block this task until the program is closed.
         await Task.Delay(-1);
-    }
-
-    private async Task EventCreated(SocketGuildEvent event1)
-    {
-        var evento = event1;
-        await CreateTextChannel(evento);
-    }
-
-    private async Task EventEnded(SocketGuildEvent event1)
-    {
-        var evento = event1;
-
-        var guild = event1.Guild;
-        var textChannel = guild.Channels.FirstOrDefault(ch => ch.Name.Equals(evento.Name, StringComparison.OrdinalIgnoreCase));
-
-        if (textChannel != null)
-        {
-            await textChannel.DeleteAsync();
-        }
-    }
-
-    private async Task CreateTextChannel(SocketGuildEvent event1)
-    {
-        var guild = event1.Guild;
-        RestCategoryChannel eventsChannel;
-        if (!guild.CategoryChannels.Any(c => c.Name == "Events"))
-        {
-            eventsChannel = await guild.CreateCategoryChannelAsync("Events");
-        }
-
-        var eventsCat = guild.CategoryChannels.FirstOrDefault(r => r.Name == "Events");
-
-        if (eventsCat == null)
-            return;
-
-        var textChannel = await guild.CreateTextChannelAsync(event1.Name, opt =>
-        {
-            opt.CategoryId = eventsCat.Id;
-            opt.Topic = event1.Description;
-        });
-
-        await textChannel.SendMessageAsync($"Welcome to {event1.Name}, a channel created for event!");
     }
 }
