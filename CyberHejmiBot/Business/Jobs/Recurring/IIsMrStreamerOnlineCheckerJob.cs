173aa0ba219c7ff8418384990341ac67c5f23746
Piotr Hejmej <piotrek10h@gmail.com>
2024-03-14 20:54:31 +0100

    back

--- CyberHejmiBot/Business/Jobs/Recurring/IIsMrStreamerOnlineCheckerJob.cs
+++ CyberHejmiBot/Business/Jobs/Recurring/IIsMrStreamerOnlineCheckerJob.cs
@@ -24,30 +24,30 @@ namespace CyberHejmiBot.Business.Jobs.Recurring
 
         public void AddOrUpdate()
         {
-            RecurringJob.AddOrUpdate<IIsMrStreamerOnlineCheckerJob>(x => x.DoWork(), "*/5 16-22 * * *");
+            RecurringJob.AddOrUpdate<IIsMrStreamerOnlineCheckerJob>(x => x.DoWork(), "*/15 16-22 * * *");
         }
 
         public async Task DoWork()
         {
-            //var latestSuccessfulCheck = await DbContext
-            //    .MrStreamerCheckerLogs
-            //    .MaxAsync(r => r.LastSuccessfullCheck);
+            var latestSuccessfulCheck = await DbContext
+                .MrStreamerCheckerLogs
+                .MaxAsync(r => r.LastSuccessfullCheck);
 
-            //if (latestSuccessfulCheck.Date == DateTime.Today.Date)
-            //    return;
+            if (latestSuccessfulCheck.Date == DateTime.Today.Date)
+                return;
 
             var isMrStreamerOnline = await TwitchChecker.IsMrStreamerOnline();
 
             if (isMrStreamerOnline.IsSuccesfull && isMrStreamerOnline.Result)
             {
-                //await ClearPreviousEntries();
-                //var log = new MrStreamerCheckerLogs
-                //{
-                //    LastSuccessfullCheck = DateTime.Now
-                //};
+                await ClearPreviousEntries();
+                var log = new MrStreamerCheckerLogs
+                {
+                    LastSuccessfullCheck = DateTime.Now
+                };
 
-                //await DbContext.AddAsync(log);
-                //await DbContext.SaveChangesAsync();
+                await DbContext.AddAsync(log);
+                await DbContext.SaveChangesAsync();
 
                 if (await Client.Rest.GetChannelAsync(CHANNEL_ID) is not RestTextChannel restChannel)
                     return;
